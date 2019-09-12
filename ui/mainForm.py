fe7ac13d44932dc9a7d3d6b4bcd6c46b0b56c63a
volodymyr bondariev <vladmikhbond@hotmail.com>
2019-09-12 14:09:46 +0300

    init

--- ui/mainForm.py
+++ ui/mainForm.py
@@ -13,11 +13,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(907, 620)
+        MainWindow.resize(800, 800)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.frame = QtWidgets.QFrame(self.centralwidget)
-        self.frame.setGeometry(QtCore.QRect(10, 10, 721, 80))
+        self.frame.setGeometry(QtCore.QRect(10, 10, 721, 51))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.frame.setFont(font)
@@ -29,13 +29,14 @@ class Ui_MainWindow(object):
         self.label.setObjectName("label")
         self.textEdit = QtWidgets.QTextEdit(self.frame)
         self.textEdit.setGeometry(QtCore.QRect(90, 10, 531, 31))
+        self.textEdit.setDocumentTitle("")
         self.textEdit.setObjectName("textEdit")
         self.okButton = QtWidgets.QPushButton(self.frame)
         self.okButton.setGeometry(QtCore.QRect(640, 20, 61, 21))
         self.okButton.setObjectName("okButton")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 907, 18))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 18))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
@@ -49,4 +50,10 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "TextLabel"))
+        self.textEdit.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:12pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">{\'x\': 100, \'y\': 0, \'vx\': 0, \'vy\': 1, }</p></body></html>"))
+        self.textEdit.setPlaceholderText(_translate("MainWindow", "12345"))
         self.okButton.setText(_translate("MainWindow", "Ok"))
--- work.py
+++ work.py
@@ -1,9 +1,10 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtGui import QPainter, QColor, QFont
-from PyQt5.QtCore import Qt, QTimer, QPoint
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtCore import QTimer, QPoint
 from Ball import Ball
 from ui.mainForm import Ui_MainWindow  # импорт нашего сгенерированного файла
+
 
 class Example(QMainWindow):
 
@@ -18,37 +19,46 @@ class Example(QMainWindow):
         self.timer = QTimer()
         self.timer.timeout.connect(self.step)
         self.timer.start(10)
-        self.ball = Ball(50, 50, 1, 1)
+        ##########################################################
+        self.ball = Ball(x=100, y=0, vx=0, vy=1)
 
+        self.ui.okButton.clicked.connect(self.okBtnClicked)
 
     def step(self):
-        self.ball.fx = 0;
-        self.ball.fy = 0.01;
-        self.ball.step()
+        k = -10000
+        b = self.ball
+        r2 = (b.x**2 + b.y**2)**2
+        b.fx = k * b.x / r2
+        b.fy = k * b.y / r2
+        b.step()
         self.repaint()
-
 
     def paintEvent(self, event):
         qp = QPainter()
-        qp.begin(self)
 
-        qp.setPen(QColor(168, 34, 3))
+        qp.begin(self)
+        qp.translate(400, 400)
+        qp.scale(1, -1)
+        # draw Center
+        qp.drawEllipse(QPoint(0, 0), 5, 5)
+        qp.drawPoint(QPoint(0, 0))
+
+        # draw ball
+        qp.setBrush(QColor(168, 34, 3))
         b = self.ball
-        qp.drawEllipse(QPoint(b.x,b.y),30,30)
+        r = 10
+        qp.drawEllipse(QPoint(b.x, b.y), r, r)
 
         qp.end()
 
-
-
-    # def drawText(self, event, qp):
-    #     qp.setPen(QColor(168, 34, 3))
-    #     qp.setFont(QFont('Decorative', 10))
-    #     qp.drawText(event.rect(), Qt.AlignCenter, self.x)
+    def okBtnClicked(self):
+        text = self.ui.textEdit.toPlainText()
+        o = eval(text)
+        self.ball.x = o['x']
+        self.ball.y = o['y']
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
-
-
