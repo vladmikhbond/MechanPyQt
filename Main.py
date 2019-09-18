c08c45e69db69dff3eff4d16a29143251e96f963
volodymyr bondariev <vladmikhbond@hotmail.com>
2019-09-18 15:05:11 +0300

    catch exceptions

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 import sys
-import math
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtGui import QPainter, QColor, QBrush, QPen, QPixmap
-from PyQt5.QtCore import QTimer, QPoint
+from PyQt5.QtGui import QColor
+from PyQt5.QtCore import QTimer, QRect
 from model.Ball import Ball
 from model.Central import Central
 from ui.mainForm import Ui_MainWindow  # импорт нашего сгенерированного файла
+from fieldWidget import FieldWidget
 
 T_INTERVAL = 20
 TRACK_COLOR = QColor('red')
@@ -13,14 +13,23 @@ BALL_COLOR = QColor('red')
 SUN_COLOR = QColor('yellow')
 
 
-class Movie(QMainWindow):
+class Main(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.fieldImage = None
+
+        # init model
+        self.model = Central(800, 800, Ball())
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        self.fieldWidget = FieldWidget(self, self.model)
+        self.fieldWidget.setGeometry(QRect(10, 110, self.model.width, self.model.height))
+        self.fieldWidget.setMouseTracking(True)
+        self.fieldWidget.setObjectName("fieldWidget")
+
+
         self.show()
 
         # init timer
@@ -31,15 +40,13 @@ class Movie(QMainWindow):
         self.setMouseTracking(True)
         self.ui.okButton.clicked.connect(self.okBtnClicked)
 
-        # init model
-        self.width()
-        self.central = Central(self.width(), self.height(), Ball())
         self.okBtnClicked()
 
     # ========================================== Handlers
 
     def mousePressEvent(self, event):
-        p = self.central.ScreenToWorld(event.pos())
+        # diagnostic print
+        p = self.model.ScreenToWorld(event.pos())
         v = Central.V(p.x(), p.y())
         print(f'x={p.x():4}  y={p.y():4}  v={v:0.4f}')
 
@@ -52,114 +59,33 @@ class Movie(QMainWindow):
     def okBtnClicked(self):
         # renew potential
         text = self.ui.potential.toPlainText()
-        if self.central.refresh(text):
-            self.createFieldImage()
+        if self.model.refresh(text):
+            self.fieldWidget.createFieldImage()
         else:
             print("ERROR")
 
         # renew a ball
         text = self.ui.conditions.toPlainText()
-        self.central.balls[0].refresh(text)
+        self.model.balls[0].refresh(text)
         self.timer.start(T_INTERVAL)
 
     def step(self):
         # stop timer when a ball is far away
-        if self.central.balls[0].r() > self.central.width * 2:
+        if self.model.balls[0].r() > self.model.width * 2:
             self.timer.stop()
 
         # many steps and one painting
         for i in range(10):
-            self.central.step()
-        self.repaint()
+            self.model.step()
+        # self.repaint()
+        self.fieldWidget.repaint()
 
         # energy diagnostic
-        e = self.central.T() + self.central.v()
+        e = self.model.T() + self.model.v()
         self.setWindowTitle(f'E = {e:12.8f}')
 
-    # ========================================== Drawing
-
-    def paintEvent(self, event):
-        qp = QPainter()
-        qp.begin(self)
-        if self.fieldImage:
-            qp.drawPixmap(0, 0, self.fieldImage)
-
-        # balls
-        qp.translate(self.central.width / 2, self.central.height / 2)
-        qp.scale(1, -1)
-        qp.setPen(BALL_COLOR)
-        qp.setBrush(BALL_COLOR)
-        r = 10
-        for b in self.central.balls:
-            qp.drawEllipse(QPoint(b.x, b.y), r, r)
-        qp.end()
-
-        # tracks to image
-        qpi = QPainter()
-        qpi.begin(self.fieldImage)
-        qpi.translate(self.central.width / 2, self.central.height / 2)
-        qpi.scale(1, -1)
-        qpi.setPen(TRACK_COLOR)
-        for b in self.central.balls:
-            if b.prevX:
-                qpi.drawLine(b.x, b.y, b.prevX, b.prevY)
-            b.prevX = b.x
-            b.prevY = b.y
-
-        qpi.end()
-
-    def createFieldImage(self):
-        self.fieldImage = QPixmap(self.central.width, self.central.height)
-        qp = QPainter()
-        qp.begin(self.fieldImage)
-        qp.translate(self.central.width / 2, self.central.height / 2)
-        qp.scale(1, -1)
-
-        w = self.central.width // 2 - 1
-        h = self.central.height // 2 - 1
-        d = 6
-        v_min = v_max = Central.V(-w, -h)
-        for x in range(-w, w, d):
-            for y in range(-h, h, d):
-                v = Central.V(x, y)
-                if v_min > v:
-                    v_min = v
-                if v_max < v:
-                    v_max = v
-        if v_max == v_min:
-            return
-
-        for x in range(-w, w, d):
-            for y in range(-h, h, d):
-                v = Central.V(x, y)
-                color = 255 - 255 * (v - v_min) / (v_max - v_min)
-                q_color = QColor(color, color,  255)
-
-                qp.setPen(q_color)
-                qp.setBrush(q_color)
-                qp.drawRect(x - d/2, y - d/2, d, d)
-
-        # draw Center
-        qp.setPen(QPen(QBrush(BALL_COLOR), 1))
-        qp.setBrush(SUN_COLOR)
-        qp.drawEllipse(QPoint(0, 0), 5, 5)
-
-        # draw V-profile
-        qp.setPen(QColor(128, 128, 128))
-        xs = range(d, w, d)
-        vs = [Central.V(x, 0) for x in xs]
-        v_min, v_max = min(*vs), max(*vs)
-        k = 255 / (v_max - v_min)
-        ns = [(v - v_min) * k for v in vs]
-        ps = [QPoint(x, n) for x, n in zip(xs, ns)]
-        qp.drawPolyline(*ps)
-
-        qp.end()
-
-
-# ========================================== Main
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Movie()
+    ex = Main()
     sys.exit(app.exec_())
---
+++ fieldWidget.py
@@ -0,0 +1,95 @@
+from PyQt5.QtWidgets import QWidget
+from model.Central import Central
+from PyQt5.QtGui import QPainter, QColor, QBrush, QPen, QPixmap
+from PyQt5.QtCore import QTimer, QPoint, QRect
+
+TRACK_COLOR = QColor('red')
+BALL_COLOR = QColor('red')
+SUN_COLOR = QColor('yellow')
+
+class FieldWidget(QWidget):
+
+    def __init__(self, owner, model):
+        super().__init__(owner)
+        self.model = model
+        self.fieldImage = None
+
+    def paintEvent(self, event):
+
+
+        qp = QPainter()
+        qp.begin(self)
+        if self.fieldImage:
+            qp.drawPixmap(0, 0, self.fieldImage)
+
+        # balls
+        qp.translate(self.model.width / 2, self.model.height / 2)
+        qp.scale(1, -1)
+        qp.setPen(BALL_COLOR)
+        qp.setBrush(BALL_COLOR)
+        r = 10
+        for b in self.model.balls:
+            qp.drawEllipse(QPoint(b.x, b.y), r, r)
+        qp.end()
+
+        # tracks to image
+        qpi = QPainter()
+        qpi.begin(self.fieldImage)
+        qpi.translate(self.model.width / 2, self.model.height / 2)
+        qpi.scale(1, -1)
+        qpi.setPen(TRACK_COLOR)
+        for b in self.model.balls:
+            if b.prevX:
+                qpi.drawLine(b.x, b.y, b.prevX, b.prevY)
+            b.prevX = b.x
+            b.prevY = b.y
+
+        qpi.end()
+
+    def createFieldImage(self):
+        self.fieldImage = QPixmap(self.model.width, self.model.height)
+        qp = QPainter()
+        qp.begin(self.fieldImage)
+        qp.translate(self.model.width / 2, self.model.height / 2)
+        qp.scale(1, -1)
+
+        w = self.model.width // 2 - 1
+        h = self.model.height // 2 - 1
+        d = 6
+        v_min = v_max = Central.V(-w, -h)
+        for x in range(-w, w, d):
+            for y in range(-h, h, d):
+                v = Central.V(x, y)
+                if v_min > v:
+                    v_min = v
+                if v_max < v:
+                    v_max = v
+        if v_max == v_min:
+            return
+
+        for x in range(-w, w, d):
+            for y in range(-h, h, d):
+                v = Central.V(x, y)
+                color = 255 - 255 * (v - v_min) / (v_max - v_min)
+                q_color = QColor(color, color,  255)
+
+                qp.setPen(q_color)
+                qp.setBrush(q_color)
+                qp.drawRect(x - d/2, y - d/2, d, d)
+
+        # draw Center
+        qp.setPen(QPen(QBrush(BALL_COLOR), 1))
+        qp.setBrush(SUN_COLOR)
+        qp.drawEllipse(QPoint(0, 0), 5, 5)
+
+        # draw V-profile
+        qp.setPen(QColor(128, 128, 128))
+        xs = range(d, w, d)
+        vs = [Central.V(x, 0) for x in xs]
+        v_min, v_max = min(*vs), max(*vs)
+        k = 255 / (v_max - v_min)
+        ns = [(v - v_min) * k for v in vs]
+        ps = [QPoint(x, n) for x, n in zip(xs, ns)]
+        qp.drawPolyline(*ps)
+
+        qp.end()
